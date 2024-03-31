4a6ab865dada6001469c46850b0a24c861e57456
Shawn Faber <96348445+sfaber02@users.noreply.github.com>
2024-03-31 00:35:48 -0400

    draw polygon working

--- app/src/main/java/com/example/android_google_maps/MapsActivity.kt
+++ app/src/main/java/com/example/android_google_maps/MapsActivity.kt
@@ -1,5 +1,6 @@
 package com.example.android_google_maps
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -9,12 +10,15 @@ import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.maps.model.PolygonOptions
+import com.google.android.gms.maps.model.Polygon
 import com.example.android_google_maps.databinding.ActivityMapsBinding
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var mMap: GoogleMap
     private lateinit var binding: ActivityMapsBinding
+    private lateinit var polygon: Polygon
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -39,10 +43,34 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
      */
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
+        val polygonPoints = mutableListOf(LatLng(-34.0, 151.0))
+        polygon = mMap.addPolygon(createPolygonOptions(polygonPoints))
+
+        // Listen to map clicks to add points to polygonOptions
+        mMap.setOnMapClickListener { latLng ->
+            // if the polygonPoints only has the initial point, replace it
+            if (polygonPoints.size == 1 && polygonPoints.first() == LatLng(0.0, 0.0)) {
+                polygonPoints[0] = latLng
+            } else {
+                // otherwise, add the new point
+                polygonPoints.add(latLng)
+            }
+
+            // then recreate the polygon with the new points
+            polygon?.remove()
+            polygon = mMap.addPolygon(createPolygonOptions(polygonPoints))
+        }
 
         // Add a marker in Sydney and move the camera
         val sydney = LatLng(-34.0, 151.0)
         mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+    }
+
+    fun createPolygonOptions(polygonPoints: MutableList<LatLng>): PolygonOptions {
+        return PolygonOptions()
+            .addAll(polygonPoints)
+            .strokeColor(Color.RED)
+            .fillColor(Color.BLUE)
     }
 }
